3bc7ac18f45819aefbb3582888a4fbaf574575e4
Austin <aharshbe@live.com>
2017-12-12 23:09:40 -0800

    Adding changes to add sharing + some copy changes

--- app/src/main/java/x/aharshbe/ahars/tappingtest/MainActivity.java
+++ app/src/main/java/x/aharshbe/ahars/tappingtest/MainActivity.java
@@ -1,6 +1,7 @@
 package x.aharshbe.ahars.tappingtest;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.v7.app.AlertDialog;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     Button face, start;
     int counter, timer_time;
     Animation myAnim;
+    String download_on_play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         instr = findViewById(R.id.instr);
         myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
         face.setBackgroundColor(Color.YELLOW);
+        download_on_play = "https://goo.gl/U83epc";
 
 
         AlertDialog.Builder builder2 = new AlertDialog.Builder(MainActivity.this);
@@ -104,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickingStart(View view) {
 
-        start.setText(" Click as fast you can! ");
+        start.setText(" Tap as fast you can! ");
         face.setVisibility(View.VISIBLE);
         instr.setVisibility(View.INVISIBLE);
         start.setClickable(false);
@@ -124,10 +127,10 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText("");
 
                 AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
-                builder1.setMessage("Your score was: " + counter);
+                builder1.setMessage("Your score was: " + counter + ". Would you like to play again or share your results?");
                 builder1.setCancelable(false);
                 builder1.setNegativeButton(
-                        "Reset",
+                        "Play again",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 counter = 0;
@@ -174,10 +177,78 @@ public class MainActivity extends AppCompatActivity {
                                             }
                                         });
 
+
+
                                 AlertDialog alert112 = builder2.create();
                                 alert112.show();
                             }
                         });
+
+                builder1.setPositiveButton(
+                        "Share",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                                sharingIntent.setType("text/plain");
+                                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "I scored a " + counter + " on Bouncy Cube within " +
+                                timer_time / 1000 + " seconds. How fast are you? Download on Google Play: " + download_on_play);
+                                startActivity(Intent.createChooser(sharingIntent,"Share using"));
+
+                                counter = 0;
+                                textView.setText(String.valueOf(counter));
+                                face.setText("-_-");
+                                face.setBackgroundColor(Color.CYAN);
+                                timer.setText(" ");
+                                dialog.cancel();
+                                start.setText("Start Game");
+                                face.setVisibility(View.INVISIBLE);
+                                instr.setVisibility(View.VISIBLE);
+                                start.setClickable(true);
+
+
+                                AlertDialog.Builder builder2 = new AlertDialog.Builder(MainActivity.this);
+                                builder2.setMessage("How long would you like to play for?");
+                                builder2.setCancelable(false);
+                                builder2.setNeutralButton(
+                                        "60 seconds",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+
+                                                timer_time = 60000;
+
+                                            }
+                                        });
+                                builder2.setNegativeButton(
+                                        "30 seconds",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+
+                                                timer_time = 30000;
+
+                                            }
+                                        });
+
+                                builder2.setPositiveButton(
+                                        "10 seconds",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+
+                                                timer_time = 10000;
+
+                                            }
+                                        });
+
+
+
+                                AlertDialog alert112 = builder2.create();
+                                alert112.show();
+                            }
+                        });
+
+
+
+
 
 
 
